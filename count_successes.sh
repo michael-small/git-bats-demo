248f6f3258a6a0944d0206912ebd0828ee293a8f
michael-small <small203@morris.umn.edu>
2019-09-02 22:05:38 -0500

    Correct script to pass all tests

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "There were 78 successes and 22 failures."
+tar_file=$1
+
+SCRATCH=`mktemp --directory`
+
+tar zxf $tar_file --directory $SCRATCH
+
+num_successes=`grep -lr SUCCESS $SCRATCH | wc -l`
+num_failures=`grep -lr FAILURE $SCRATCH | wc -l`
+
+echo "There were $num_successes successes and $num_failures failures."
+
+rm -rf $SCRATCH
